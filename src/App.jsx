9b0bfa070282e5a9b49bfe716fdcc3ea5a83665b
Chunchunmaru25 <clarenceduerme40@gmail.com>
2026-03-27 10:55:07 +0800

    feat: initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Routes, Route } from 'react-router-dom'
+import Welcome from './pages/welcome'
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path='/' element={<Welcome />} />
+      </Routes>
+    </>
+  )
+}
+
+export default App;
---
+++ src/pages/welcome.jsx
@@ -0,0 +1,9 @@
+function Welcome() {
+    return (
+        <>
+            <h1>asdkjahujsdas</h1>
+        </>
+    )
+}
+
+export default Welcome;
